3930dc44f2a4e53c37e2058aae7de53e7ff0383b
Denis Souza <denyssouza1@hotmail.com>
2026-05-30 18:56:23 -0300

    Configure swagger

--- Time.Api/Program.cs
+++ Time.Api/Program.cs
@@ -3,11 +3,14 @@ using Scalar.AspNetCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapOpenApi();
-app.MapScalarApiReference();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
